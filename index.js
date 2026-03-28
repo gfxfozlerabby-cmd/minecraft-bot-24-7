54714e821f9a1a1d9f075f0009d649dce5864f7f
gfxfozlerabby-cmd <gfxfozlerabby@gmail.com>
2026-03-28 14:58:23 +0600

    index.js

--- index.js
+++ index.js
@@ -12,53 +12,28 @@ function createBot() {
     bot.loadPlugin(pathfinder);
 
     bot.on('spawn', () => {
-        console.log('বট সচল! 🏃‍♂️');
+        console.log('বট এখন লাইভ! 🏃‍♂️');
         bot.chat('/tp Justice_Player 861 76 -1014');
     });
 
-    // বের হয়ে গেলে আবার ঢুকবে
+    // রাত হলে ঘুমানোর অংশ
+    bot.on('time', () => {
+        const time = bot.time.timeOfDay;
+        if (time >= 13000 && time <= 23000) {
+            if (!bot.isSleeping) {
+                const bed = bot.findBlock({
+                    matching: block => bot.isABed(block),
+                    maxDistance: 10
+                });
+                if (bed) bot.sleep(bed).catch(() => {});
+            }
+        } else if (bot.isSleeping) {
+            bot.wake().catch(() => {});
+        }
+    });
+
     bot.on('end', () => setTimeout(createBot, 5000));
-    bot.on('error', (err) => console.log('Log:', err.message));
+    bot.on('error', (err) => console.log('Error:', err.message));
 }
 
 createBot();
-    bot.loadPlugin(pathfinder);
-
-    bot.on('spawn', () => {
-        console.log('নুব বট এখন ঘুমাতেও জানে! 💤');
-        const mcData = require('minecraft-data')(bot.version);
-        const movements = new Movements(bot, mcData);
-        bot.pathfinder.setMovements(movements);
-        
-        // বাড়িতে টেলিপোর্ট
-        bot.chat('/tp Justice_Player 861 76 -1014');
-
-        // --- নুব স্টাইল অগোছালো কাজ (দিনের বেলা) ---
-        setInterval(() => {
-            if (bot.isSleeping) return; // ঘুমানোর সময় ডিস্টার্ব করবে না
-
-            const rand = Math.random();
-            if (rand < 0.25) {
-                // ১. অকারণে এদিক সেদিক হাঁটা (৮ ব্লকের মধ্যে)
-                const x = 861 + (Math.floor(Math.random() * 16) - 8);
-                const z = -1014 + (Math.floor(Math.random() * 16) - 8);
-                bot.pathfinder.setGoal(new goals.GoalNear(x, 76, z, 1));
-            } else if (rand < 0.5) {
-                // ২. অকারণে লাফানো আর আকাশ দেখা
-                bot.setControlState('jump', true);
-                bot.look(Math.random() * 6, (Math.random() - 0.5) * 1.5);
-                setTimeout(() => bot.setControlState('jump', false), 500);
-            } else if (rand < 0.7) {
-                // ৩. সামনের ব্লককে অকারণে ঘুষি মারা (নুব স্টাইল)
-                const targetBlock = bot.blockAtCursor(4);
-                if (targetBlock && targetBlock.name !== 'air') {
-                    bot.dig(targetBlock).catch(() => {});
-                }
-            }
-        }, 7000); 
-    });
-
-    // --- ৪. অটো ঘুমানোর স্ক্রিপ্ট ---
-    bot.on('time', () => {
-        const time = bot.time.timeOfDay;
-        if (time >=
